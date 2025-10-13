ac11b3630142eee4d52b909fe6f6db809dc2bedb
Andrew Apple <andrewdapple@gmail.com>
2025-10-12 21:56:08 -0400

    Create 009_create_post_tags_table.sql

---
+++ scripts/009_create_post_tags_table.sql
@@ -0,0 +1,7 @@
+-- 009_create_post_tags_table.sql
+CREATE TABLE post_tags (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  post_id uuid REFERENCES posts(id) ON DELETE CASCADE,
+  tagged_user_id uuid REFERENCES profiles(id) ON DELETE CASCADE,
+  created_at timestamptz DEFAULT now()
+);
